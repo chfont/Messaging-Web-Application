b7ee13a01fb5d335dfa25c3418cb1b8e96b46673
leafBlader <zye3@lsu.edu>
2019-02-02 13:26:42 -0600

    Modify login.css, login.html, settings.html, views.py

--- djangoproject/djangoproject/views.py
+++ djangoproject/djangoproject/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render, redirect
 import pyrebase
 from .forms import LoginForm, NewUser
 config = {
-    "apiKey": "AIzaSyBYm7w0kt_NvWmH9GE1vI9ckN9SrXdHJgw",
-    "authDomain": "loginauth-80e28.firebaseapp.com",
-    "databaseURL": "https://loginauth-80e28.firebaseio.com",
-    "projectId": "loginauth-80e28",
-    "storageBucket": "loginauth-80e28.appspot.com",
-    "messagingSenderId": "1018199481520"
+    "null"
 
 }
 
