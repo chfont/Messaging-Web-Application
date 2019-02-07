45cfad7fc4bafdccc2ad5abf33e0b1dbdda9c5ea
mango <thatonemango813@gmail.com>
2019-02-07 07:47:10 -0600

    Add classes for dealing with messages and conversations

---
+++ djangoproject/djangoproject/messages.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+class Message
+
+    def __init__(self, s, m)
+        self.sender = s
+        self.timeStamp = datetime.now()
+        self.text = m
+
+
+class Conversation
+
+    def __init(self, t, k)
+        self.title = t
+        self.key = k
+        
