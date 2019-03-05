2167e77fd6e7e6704e542128ce1907e376c3ecfc
mango <thatonemango813@gmail.com>
2019-03-05 17:02:03 -0600

    Update pollConvs (should have been in previous commit)

--- djangoproject/djangoproject/database.py
+++ djangoproject/djangoproject/database.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from time import time
 from .messages import *
 from Crypto import Random
+from Crypto.Hash import SHA256
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
@@ -20,16 +21,17 @@ def retrieveUserData(id):
 def addConv(id, convT, convK, convRs, username):
     reps = convRs.split(",")
     reps.append(username)
-    data = {"name": convT, "key": convK, "lastSent": time()}
+    data = {"name": convT, "key": convK, "lastSent": time(),"recipients": reps}
     data2 = {"name": convT, "lastSent": time(), "recipients": reps}
-#    db.child('users').child(id).child("Conversations").child(data['name']).set(data2)
+
     rnd = Random.new()
     byt = rnd.read(16)
     byt = int.from_bytes(byt, byteorder='little')
     #check validity here
     data3 = retrieveUserData(id)
+    db.child('users').child(id).child("Conversations").child(byt).set(data)
     db.child('Conversations').child(byt).set(data2)
-    c = Conversation(data['name'], data['lastSent'], data2['recipients'])
+    c = Conversation(data['name'], data['lastSent'], data2['recipients'],byt)
     return c
 
 def getConvs(id,username):
@@ -52,8 +54,15 @@ def pollConvs(id, username):
     for i in convList:
         if (username in convList[i]['recipients']):
             data = {"chatID": i, "recipients": convList[i]['recipients'],"name": convList[i]['name'], "lastSent": convList[i]['lastSent']}
-            db.child("users").child(id).child("Conversations").child(i).set(data)
+            c = db.child("users").child(id).child("Conversations").child(i).get().val()
+            if c == None:
+                k = ""
+                enc = SHA256.new(k.encode('utf-8')).hexdigest()
+                data2 = data = {"chatID": i, "recipients": convList[i]['recipients'],"name": convList[i]['name'], "lastSent": convList[i]['lastSent'], "key": enc}
+                db.child("users").child(id).child("Conversations").child(i).set(data2)
+            else:
+                db.child("users").child(id).child("Conversations").child(i).update(data)
             print("placeholder")
-            c = Conversation(convList[i]['name'],convList[i]['lastSent'], convList[i]['recipients'])
+            c = Conversation(convList[i]['name'],convList[i]['lastSent'], convList[i]['recipients'], i)
             convs.append(c)
     return convs
